3ef907ffd6483b7063901a1b081456e0a9d1b0ad
nzuluu <nisayblu2@gmail.com>
2026-03-21 10:43:35 -0700

    created a basic HTML calculator

--- my-script.js
+++ my-script.js
@@ -9,9 +9,6 @@ const subtract = function(a,b) {
 const multiply = function(a,b) {
     return a * b;
 };
-// const multiply = function(arr) {
-// 	return arr.reduce((sum,current) => sum * current);
-// };
 
 const divide = function(a,b)  {
     return a/b;
@@ -40,3 +37,81 @@ const factorial = function(a) {
     return "number has to be positive";
   }
 };
+
+const operate = function(a, operator, b) {
+    switch(operator) {
+        case "+":
+            return add(a,b);
+        case "-":
+            return subtract(a,b);
+        case "*":
+            return multiply(a,b);
+        case "/":
+            return divide(a,b);
+        default:
+            alert('Enter a valid operator');
+    }
+};
+
+function createBtns() {
+    const container = document.querySelector("#container");
+    const userInput = "1 + 5";
+    const displayInput = document.createElement("div");
+    displayInput.classList.add("display");
+    displayInput.innerHTML = userInput;
+    container.appendChild(displayInput);
+
+    const btnContainer = document.createElement("div");
+    btnContainer.classList.add("btnContainer");
+
+    for (let i = 0; i <= 9; i++) {
+        const btn = document.createElement("button");
+        btn.classList.add("num");
+        btn.textContent = `${9-i}`;
+        btnContainer.appendChild(btn);
+    }
+    const decimal = document.createElement("button");
+    decimal.classList.add("decimal");
+    decimal.textContent = ".";
+    btnContainer.appendChild(decimal);
+
+    const clearBtn = document.createElement("button");
+    clearBtn.classList.add("clear");
+    clearBtn.textContent = "Clear";
+    btnContainer.appendChild(clearBtn);
+
+    const operates = document.createElement("div");
+    operates.classList.add("oppContainer");
+
+    const divideBtn = document.createElement("button");
+    divideBtn.classList.add("divide");
+    divideBtn.innerHTML = "&#247";
+
+    const multipleBtn = document.createElement("button");
+    multipleBtn.classList.add("multiple");
+    multipleBtn.innerHTML = "&#215";
+
+    const minusBtn = document.createElement("button");
+    minusBtn.classList.add("minus");
+    minusBtn.innerHTML = "&#8722";
+
+    const plusBtn = document.createElement("button");
+    plusBtn.classList.add("plus");
+    plusBtn.innerHTML = "&#43";
+
+    const equalBtn = document.createElement("button");
+    equalBtn.classList.add("equal");
+    equalBtn.innerHTML = "&#61;";
+
+    operates.appendChild(divideBtn);
+    operates.appendChild(multipleBtn);
+    operates.appendChild(minusBtn);
+    operates.appendChild(plusBtn);
+    operates.appendChild(equalBtn);
+
+
+    container.appendChild(btnContainer);
+    container.appendChild(operates);
+}
+
+createBtns();
